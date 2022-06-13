71b62255b492be861fd3cc07deaa5a04a5bb2803
EC2 Default User <kazukipump7@gmail.com>
2022-06-13 13:13:07 +0000

    update second

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  def index
+    @book = Book.new
+  end
+  
+  def show
+    @booknew = Book.new
+  end
+  
+  def destroy
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  root to: 'homes#top'
+  resources :books,only:[:create,:index,:show,:destroy,:edit]
+  get 'homes/about'=> 'homes#about',as: :about
+
+  resources :users, only:[:show, :edit, :index,:update]
 end
